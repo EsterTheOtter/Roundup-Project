f1bf53d3b6f858b653d9ee0542e6c68081fc8361
EsterMeritStark <>
2022-03-16 19:22:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// Roundup Project
+
+/* Objective: Create a program that ROUNDS UP any decimal to the next whole
+number and console.log the result. */
+/*
+let x=
+let y=
+const c= x*7.25+y*5.4 
+
+console.log(Math.ceil(c)) */
+
+// Calculating Gravity on different planets
+// F = m * a
+// Force on any planet
+// Mass is in kg, to convert to pounds, multiply by 2.20462262185
+// kg to lbs
+/*
+const f =
+const m = 
+
+
+console.log()
+*/
+
+// Pluto Example
+
+//Solving for fg
+/*let fg =*/
+//les gravitational constant
+let g = 6.673 * 10^-11
+let m1 = 0.0125 * 10^24
+let m2 = 44
+let r = 1195000^2
+
+console.log(g)
